b541c736a33dfac990685c48aa30106dd07fbe91
xuchenbiao <2841694242@qq.com>
2022-11-09 16:42:34 +0800

    好了，登陆注册都好了plus

--- src/main/java/com/itheima/controller/ConfigController.java
+++ src/main/java/com/itheima/controller/ConfigController.java
@@ -1,5 +1,6 @@
 package com.itheima.controller;
 import com.itheima.dao.UserDao;
+import com.itheima.domain.User;
 import com.itheima.util.R;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,21 +14,21 @@ public class ConfigController {
     @Autowired
     private UserDao userDao;
 
-    @PostMapping("/{username}/{password}")
-    public R Login1(@PathVariable String username, @PathVariable String password, HttpSession session) {
+    @PostMapping
+    public R Login1(@RequestBody User user, HttpSession session) {
         List<String> list1 = userDao.getUsername();
         List<String> list2 = userDao.getPassword();
         int flag = 5;
         for (int i = 0; i < list1.size(); i++) {
-            if (username.equals(list1.get(i))) {
-                if (password.equals(list2.get(i))) {
+            if (user.getUsername().equals(list1.get(i))) {
+                if (user.getPassword().equals(list2.get(i))) {
                     flag = 1;
                     break;
                 }
             } else flag = 0;
         }
         if (flag == 1) {
-            session.setAttribute("loginUser", username);
+            session.setAttribute("loginUser", user.getUsername());
             session.setMaxInactiveInterval(5);
             return new R(true, "登录成功");
         } else return new R(false, "您输入的信息有误，请重新输入");
--- src/main/java/com/itheima/controller/EnrollController.java
+++ src/main/java/com/itheima/controller/EnrollController.java
@@ -1,13 +1,11 @@
 package com.itheima.controller;
 
 import com.itheima.dao.EnrollDao;
+import com.itheima.domain.User;
 import com.itheima.service.EnrollService;
 import com.itheima.util.R;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,26 +16,26 @@ public class EnrollController {
    private EnrollDao enrollDao;
     @Autowired
     private EnrollService enrollService;
-    @PostMapping("/{username}/{password}")
-    public R Enroll(@PathVariable String username, @PathVariable String password){
+    @PostMapping
+    public R Enroll(@RequestBody User user){
           int k=0;
         List<String> list=enrollDao.getAllName();
        for (int i=0;i<list.size();i++){
-          if (list.get(i).equals(username)){k=1;break;}
+          if (list.get(i).equals(user.getUsername())){k=1;break;}
           else k=0;
        }
        if (k==1){
            return  new R(false,"注册失败!用户信息已存在");
        }
        else{
-          if(username.length()<8||username.length()>16){
+          if(user.getUsername().length()<8||user.getUsername().length()>16){
               return new R(false,"账号格式不能低于8位或者高于16位");
           }
-          else if(password.length()<6||password.length()>18){
+          else if(user.getPassword().length()<6||user.getPassword().length()>18){
               return new R(false,"密码格式不能低于6位或高于18位");
           }
           else
-              return  new R(enrollService.insert(username,password),"注册成功");
+              return  new R(enrollService.insert(user.getUsername(), user.getPassword()),"注册成功");
        }
 
     }
