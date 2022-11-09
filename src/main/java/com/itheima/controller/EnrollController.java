c1d5d8b7c49dfd67cce2765bd00b19c2c046892f
xuchenbiao <2841694242@qq.com>
2022-11-09 16:51:51 +0800

    优化了一下

--- src/main/java/com/itheima/controller/EnrollController.java
+++ src/main/java/com/itheima/controller/EnrollController.java
@@ -28,12 +28,12 @@ public class EnrollController {
            return  new R(false,"注册失败!用户信息已存在");
        }
        else{
-          if(user.getUsername().length()<8||user.getUsername().length()>16){
-              return new R(false,"账号格式不能低于8位或者高于16位");
+          if(user.getUsername().length()<8||user.getUsername().length()>16||
+            user.getPassword().length()<8||user.getPassword().length()>16)
+          {
+              return new R(false,"账号或密码格式不能低于8位或者高于16位");
           }
-          else if(user.getPassword().length()<6||user.getPassword().length()>18){
-              return new R(false,"密码格式不能低于6位或高于18位");
-          }
+
           else
               return  new R(enrollService.insert(user.getUsername(), user.getPassword()),"注册成功");
        }
