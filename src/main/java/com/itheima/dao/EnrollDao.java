45fe5baa464b58eb6aa5b473387ee739978e8a1b
xuchenbiao <2841694242@qq.com>
2022-11-10 20:24:47 +0800

    去md看

--- src/main/java/com/itheima/dao/EnrollDao.java
+++ src/main/java/com/itheima/dao/EnrollDao.java
@@ -2,7 +2,6 @@ package com.itheima.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.itheima.domain.User;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
--- src/test/java/com/itheima/P1ApplicationTests.java
+++ src/test/java/com/itheima/P1ApplicationTests.java
@@ -80,8 +80,5 @@ private EnrollDao enrollDao;
     public void get8(){
         enrollDao.getAllName();
     }
-//    @Test
-//    public void get9(){
-//        enrollDao.insert("6666","8888");
-//    }
+
 }
