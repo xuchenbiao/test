9fddd1b56c615d0822a6d1df3cd6a43571192bfe
xuchenbiao <2841694242@qq.com>
2022-11-12 17:22:38 +0800

    可以了，后端收工..

--- src/main/java/com/itheima/P1Application.java
+++ src/main/java/com/itheima/P1Application.java
@@ -8,8 +8,8 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@Slf4j
-@ServletComponentScan
+
+
 public class P1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(P1Application.class, args);
--- src/main/java/com/itheima/config/MyMvcConfig.java
+++ src/main/java/com/itheima/config/MyMvcConfig.java
@@ -2,6 +2,7 @@ package com.itheima.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,10 +11,14 @@ public class MyMvcConfig implements WebMvcConfigurer {
     @Autowired
   private   LoginHandlerInterceptor loginHandlerInterceptor;
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-registry.addInterceptor(loginHandlerInterceptor).addPathPatterns("/pages/books.html","/pages/ReturnBooks.html");
-    //    registry.addInterceptor(loginHandlerInterceptor).addPathPatterns("/books","/borrowOver");
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//registry.addInterceptor(loginHandlerInterceptor).addPathPatterns("/pages/books.html","/pages/ReturnBooks.html");
+    //}
 
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+       registry.addMapping("/**").allowedHeaders("*").allowedMethods("*").maxAge(1800).allowedOrigins("*");
     }
 }
